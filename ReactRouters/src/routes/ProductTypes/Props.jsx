6d5d94c20823de1faaf9b525a0c2e9d26f97ca69
vitoriadz <mariavitoriadz@gmail.com>
2023-09-23 17:42:43 -0300

    adding html and css to the props and sale routes

--- ReactRouters/src/routes/ProductTypes/Props.jsx
+++ ReactRouters/src/routes/ProductTypes/Props.jsx
@@ -4,39 +4,172 @@ import { Link } from "react-router-dom";
 
 const Props = () => {
   return (
-    <>
+    <div className="main">
     <header className="main-header">
         <div className="logo">
         <Link to="/"> <img src="src/assets/logo.png" alt="Logo" /> </Link>
         </div>
         <div className="icons">
-          <a href="#">
             <Link to="/login">
               <i
                 className="bx bx-user bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
-          <a href="#">
             <Link to="/wishlist">
               <i
                 className="bx bx-heart bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
-          <a href="#">
             <Link to="/cart">
               <i
                 className="bx bx-cart bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
         </div>
       </header>
-      <h1>Acessórios</h1>
+
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="O QUE VOCÊ ESTÁ BUSCANDO?"
+        />
+        <button className="search-button" type="submit">
+          <i className="bx bx-search"></i>
+        </button>
+      </div>
+
+      <div className="title-section">
+      <h1 className="general-title">ACESSÓRIOS</h1>
+      <hr className="hr-sections"></hr>
+      </div>
+      
+      <div className="product-grid">
+        <div className="product">
+            <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 1</div>
+            <div className="product-price">R$19.99</div>
+        </div>
+
+        <div className="product">
+            <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 2</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 3</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 4</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 5</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 6</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 7</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 8</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 9</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 10</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 11</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 12</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 13</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 14</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 15</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 16</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 17</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 18</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 19</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 20</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+    </div>
+
+      
       <footer>
         <section className="footer-section">
           <div className="footer-section-div">
@@ -46,38 +179,26 @@ const Props = () => {
           <div className="footer-section-div">
             <h3>SOBRE NÓS</h3>
             <li>
-              <a href="#">
                 <Link to="/company">A EMPRESA</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/physicalstore">CONHEÇA NOSSA LOJA FÍSICA</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/partners">NOSSOS PARCEIROS</Link>
-              </a>
             </li>
           </div>
 
           <div className="footer-section-div">
             <h3>SUPORTE</h3>
             <li>
-              <a href="#">
                 <Link to="/services">ATENDIMENTO</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/exchanges">TROCAS E DEVOLUÇÕES</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/sitemap">MAPA DO SITE</Link>
-              </a>
             </li>
           </div>
 
@@ -95,7 +216,11 @@ const Props = () => {
           </div>
         </section>
       </footer>
-    </>
+
+      <div className="last-text">
+        <p className="text-sub-footer">maremansa</p>
+      </div>
+    </div>
   );
 };
 
--- ReactRouters/src/routes/ProductTypes/Sale.jsx
+++ ReactRouters/src/routes/ProductTypes/Sale.jsx
@@ -1,42 +1,175 @@
 import React from "react";
-import "../../styles/ProductTypes/Sale.css";
+import "../../styles/ProductTypes/Props.css";
 import { Link } from "react-router-dom";
 
-const Sale = () => {
+const Props = () => {
   return (
-    <>
+    <div className="main">
     <header className="main-header">
         <div className="logo">
         <Link to="/"> <img src="src/assets/logo.png" alt="Logo" /> </Link>
         </div>
         <div className="icons">
-          <a href="#">
             <Link to="/login">
               <i
                 className="bx bx-user bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
-          <a href="#">
             <Link to="/wishlist">
               <i
                 className="bx bx-heart bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
-          <a href="#">
             <Link to="/cart">
               <i
                 className="bx bx-cart bt-header"
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-          </a>
         </div>
       </header>
-      <h1>Promoções</h1>
+
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="O QUE VOCÊ ESTÁ BUSCANDO?"
+        />
+        <button className="search-button" type="submit">
+          <i className="bx bx-search"></i>
+        </button>
+      </div>
+
+      <div className="title-section">
+      <h1 className="general-title">PROMOÇÕES</h1>
+      <hr className="hr-sections"></hr>
+      </div>
+      
+      <div className="product-grid">
+        <div className="product">
+            <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 1</div>
+            <div className="product-price">R$19.99</div>
+        </div>
+
+        <div className="product">
+            <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 2</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 3</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 4</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 5</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 6</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 7</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 8</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 9</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 10</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 11</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 12</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 13</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 14</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 15</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 16</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 17</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 18</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 19</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+
+        <div className="product">
+        <img src="src/assets/modelo_1.png" alt="" />
+            <div className="product-name">Produto 20</div>
+            <div className="product-price">R$24.99</div>
+        </div>
+    </div>
+
+      
       <footer>
         <section className="footer-section">
           <div className="footer-section-div">
@@ -46,38 +179,26 @@ const Sale = () => {
           <div className="footer-section-div">
             <h3>SOBRE NÓS</h3>
             <li>
-              <a href="#">
                 <Link to="/company">A EMPRESA</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/physicalstore">CONHEÇA NOSSA LOJA FÍSICA</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/partners">NOSSOS PARCEIROS</Link>
-              </a>
             </li>
           </div>
 
           <div className="footer-section-div">
             <h3>SUPORTE</h3>
             <li>
-              <a href="#">
                 <Link to="/services">ATENDIMENTO</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/exchanges">TROCAS E DEVOLUÇÕES</Link>
-              </a>
             </li>
             <li>
-              <a href="#">
                 <Link to="/sitemap">MAPA DO SITE</Link>
-              </a>
             </li>
           </div>
 
@@ -95,8 +216,12 @@ const Sale = () => {
           </div>
         </section>
       </footer>
-    </>
+
+      <div className="last-text">
+        <p className="text-sub-footer">maremansa</p>
+      </div>
+    </div>
   );
 };
 
-export default Sale;
+export default Props;
