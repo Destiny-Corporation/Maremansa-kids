c5627e1c329cd888092d6513b80674e4b6b2ac3d
vitoriadz <mariavitoriadz@gmail.com>
2023-09-23 20:18:25 -0300

    editing sale and props routes; creating sitemap route structure

--- ReactRouters/src/routes/ProductTypes/Props.jsx
+++ ReactRouters/src/routes/ProductTypes/Props.jsx
@@ -42,132 +42,264 @@ const Props = () => {
         </button>
       </div>
 
+      <div className="carousel-container">
+        <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active btc"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+
+            <div className="carousel-item">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+
+            <div className="carousel-item">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
       <div className="title-section">
       <h1 className="general-title">ACESSÓRIOS</h1>
       <hr className="hr-sections"></hr>
       </div>
       
-      <div className="product-grid">
-        <div className="product">
+      <div className="container-clothes">
+        <div className="clothes">
+          <Link to="/product">
             <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 1</div>
-            <div className="product-price">R$19.99</div>
+            <h6 className="text-card">PRODUTO 1</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_2.png" alt="" />
+            <h6 className="text-card">PRODUTO 2</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_3.png" alt="" />
+            <h6 className="text-card">PRODUTO 3</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_4.png" alt="" />
+            <h6 className="text-card">PRODUTO 4</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+      </div>
+
+      <div className="container-clothes">
+      <div className="clothes">
+          <Link to="/product">
             <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 2</div>
-            <div className="product-price">R$24.99</div>
+            <h6 className="text-card">PRODUTO 5</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 3</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_2.png" alt="" />
+            <h6 className="text-card">PRODUTO 6</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 4</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_3.png" alt="" />
+            <h6 className="text-card">PRODUTO 7</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 5</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_4.png" alt="" />
+            <h6 className="text-card">PRODUTO 8</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+      </div>
+
+      <div className="container-clothes">
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_1.png" alt="" />
+          </Link>
+          <Link to="/product">
+            <h6 className="text-card">PRDOUTO 9</h6>
+          </Link>
+          <h8 className="text-card">R$ 37,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 6</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_2.png" alt="" />
+            <h6 className="text-card">PRODUTO 10</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 7</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_3.png" alt="" />
+            <h6 className="text-card">PRODUTO 11</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 8</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_4.png" alt="" />
+            <h6 className="text-card">PRODUTO 12</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+      </div>
+
+      <div className="container-clothes">
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_1.png" alt="" />
+          </Link>
+          <Link to="/product">
+            <h6 className="text-card">PRODUTO 13</h6>
+          </Link>
+          <h8 className="text-card">R$ 37,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 9</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_2.png" alt="" />
+            <h6 className="text-card">PRODUTO 14</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 10</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_3.png" alt="" />
+            <h6 className="text-card">PRODUTO 15</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 11</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_4.png" alt="" />
+            <h6 className="text-card">PRODUTO 16</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
+        </div>
+      </div>
+
+      <div className="container-clothes">
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_1.png" alt="" />
+          </Link>
+          <Link to="/product">
+            <h6 className="text-card">PRODUTO 17</h6>
+          </Link>
+          <h8 className="text-card">R$ 37,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 12</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_2.png" alt="" />
+            <h6 className="text-card">PRODUTO 18</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 13</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_3.png" alt="" />
+            <h6 className="text-card">PRODUTO 19</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
 
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 14</div>
-            <div className="product-price">R$24.99</div>
+        <div className="clothes">
+          <Link to="/product">
+            <img src="src/assets/modelo_4.png" alt="" />
+            <h6 className="text-card">PRODUTO 20</h6>
+          </Link>
+          <h8 className="text-card">R$ 59,90</h8>
         </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 15</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 16</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 17</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 18</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 19</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-
-        <div className="product">
-        <img src="src/assets/modelo_1.png" alt="" />
-            <div className="product-name">Produto 20</div>
-            <div className="product-price">R$24.99</div>
-        </div>
-    </div>
+      </div>
 
       
       <footer>
--- ReactRouters/src/routes/Support/Sitemap.jsx
+++ ReactRouters/src/routes/Support/Sitemap.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Sitemap = () => {
   return (
-    <>
-    <header className="main-header">
+    <div className='main'>
+      <header className="main-header">
         <div className="logo">
-        <Link to="/"> <img src="src/assets/logo.png" alt="Logo" /> </Link>
+          <img src="src/assets/logo.png" alt="Logo" />
         </div>
         <div className="icons">
           <a href="#">
@@ -36,9 +36,129 @@ const Sitemap = () => {
           </a>
         </div>
       </header>
-      <h1>Mapa do site</h1>
-    
-      <footer>
+
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="O QUE VOCÊ ESTÁ BUSCANDO?"
+        />
+        <button className="search-button" type="submit">
+          <i className="bx bx-search"></i>
+        </button>
+      </div>
+
+      <div className="carousel-container">
+        <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active btc"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+
+            <div className="carousel-item">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+
+            <div className="carousel-item">
+              <img
+                src="src/assets/banner.png"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
+      <div className="title-section">
+      <h1 className="general-title">MAPA DO SITE</h1>
+      <hr className="hr-sections"></hr>
+      </div>
+
+      <p className="map-text">Reunimos aqui todas as novidades e tendências que a Maremansa Kids tem para você.
+             Encontre tudo em moda praia feminina e masculina que vai do infantil ao teen. Além de acessórios que farão um 
+             complemento fabuloso aos lookinhos de verão do seu pequeno. Confira!</p>
+
+    <div className="site-map">
+        <ul>     
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/female">Feminino</Link>
+                <ul>
+                    <li className="sub-list"><Link to="/">Baby</Link></li>
+                    <li className="sub-list"><Link to="/">Infantil</Link></li>
+                    <li className="sub-list"><Link to="/">Juvenil</Link></li>
+                </ul>
+            </li>
+            <li><Link to="/male">Masculino</Link>
+                <ul>
+                    <li className="sub-list"><Link to="/">Baby</Link></li>
+                    <li className="sub-list"><Link to="/">Infantil</Link></li>
+                    <li className="sub-list"><Link to="/">Juvenil</Link></li>
+                </ul>
+            </li>
+            <li><Link to="/sale">Promoções</Link></li>
+            <li><Link to="/props">Acessórios</Link></li>
+        </ul>
+    </div>
+
+
+      <footer style={{position: "static" }}>
         <section className="footer-section">
           <div className="footer-section-div">
             <img src="src/assets/baleia.png" />
@@ -96,7 +216,11 @@ const Sitemap = () => {
           </div>
         </section>
       </footer>
-    </>
+
+      <div className="last-text">
+        <p className="text-sub-footer">maremansa</p>
+      </div>
+    </div>
   );
 };
 
