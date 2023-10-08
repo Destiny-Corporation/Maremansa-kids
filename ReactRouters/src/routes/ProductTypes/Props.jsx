aeedc7bb0c3edffc0e3a7052006ef7b11792c35d
caiovieira <95643550+caioo1@users.noreply.github.com>
2023-10-08 15:00:15 -0300

    choose your pagination

--- ReactRouters/src/routes/ProductTypes/Props.jsx
+++ ReactRouters/src/routes/ProductTypes/Props.jsx
@@ -18,12 +18,12 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const firestore = getFirestore(app);
-const itemsPerPage = 12; // Número de produtos por página
 
 const Props = () => {
   const [produtos, setProdutos] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchProdutos = async () => {
@@ -42,7 +42,10 @@ const Props = () => {
 
   const pageCount = Math.ceil(filteredProdutos.length / itemsPerPage);
   const offset = currentPage * itemsPerPage;
-  const currentPageProdutos = filteredProdutos.slice(offset, offset + itemsPerPage);
+  const currentPageProdutos = filteredProdutos.slice(
+    offset,
+    offset + itemsPerPage
+  );
   const prevButtonClass =
     currentPage === 0 ? "prevButton disabled" : "prevButton";
   const nextButtonClass =
@@ -114,6 +117,18 @@ const Props = () => {
         <div className="title-section">
           <h1 className="general-title">ACESSÓRIOS</h1>
           <hr className="hr-sections"></hr>
+        </div>
+
+        <div className="items-per-page">
+          <label>Itens por página:</label>
+          <select
+            value={itemsPerPage}
+            onChange={(e) => setItemsPerPage(Number(e.target.value))}
+          >
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+          </select>
         </div>
 
         <div className="container-clothes">
