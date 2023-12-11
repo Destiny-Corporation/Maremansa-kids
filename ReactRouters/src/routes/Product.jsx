f8b2df8cc79e830603eba60a472d5d9792df4f70
caiovieira <95643550+caioo1@users.noreply.github.com>
2023-12-11 19:56:34 -0300

    changing_product

--- ReactRouters/src/routes/Product.jsx
+++ ReactRouters/src/routes/Product.jsx
@@ -25,6 +25,26 @@ export const firestore = getFirestore(app);
 // Restante do seu código...
 
 const Cart = () => {
+  const [isItemAdded, setIsItemAdded] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
+  const [showNotification2, setShowNotification2] = useState(false);
+
+  const showAddedToCartNotification = () => {
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 2000);
+  };
+
+  const showAddedToFavoriteNotification = () => {
+    setShowNotification2(true);
+    setTimeout(() => {
+      setShowNotification2(false);
+    }, 2000);
+  };
+
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
   const [showDescription, setShowDescription] = useState(false);
   const [productLoading, setProductLoading] = useState(true);
   const [selectedColor, setSelectedColor] = useState("");
@@ -40,6 +60,10 @@ const Cart = () => {
     setSelectedColor(color);
   };
 
+  const [selectedImage, setSelectedImage] = useState(
+    productData ? productData.url_image : ""
+  );
+  const [isAddToCartDisabled, setIsAddToCartDisabled] = useState(true);
   const handleSizeSelect = (size) => {
     setSelectedSize(size);
   };
@@ -52,9 +76,84 @@ const Cart = () => {
   });
   const [cartVisible, setCartVisible] = useState(false);
   const [isComponentReady, setIsComponentReady] = useState(false);
+
   const handleCartIconClick = () => {
     setCartVisible(!cartVisible);
+    setOverlayVisible(!cartVisible);
   };
+
+  const handleCloseCartClick = () => {
+    setCartVisible(false);
+    setOverlayVisible(false);
+  };
+
+  const [userVisible, setUserVisible] = useState(false);
+
+  const handleUserIconClick = () => {
+    setUserVisible(!userVisible);
+    setOverlayVisible(!userVisible);
+  };
+
+  const handleCloseUserClick = () => {
+    setUserVisible(false);
+    setOverlayVisible(false);
+  };
+
+  useEffect(() => {
+    // Recupera os itens do carrinho do localStorage ao carregar a página
+    const savedCartItems = localStorage.getItem("cartItems");
+    if (savedCartItems) {
+      setCartItems(JSON.parse(savedCartItems));
+    }
+  }, []); // O segundo argumento vazio garante que este efeito seja executado apenas uma vez, após a montagem inicial do componente.
+
+  const handleAddToCart = (produto) => {
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item.productName === productName
+    );
+
+    if (existingItemIndex !== -1) {
+      // Se o item já está no carrinho, aumente a quantidade
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].quantidade += 1;
+      setCartItems(updatedCartItems);
+    } else {
+      // Se o item não está no carrinho, adicione-o com quantidade 1
+      setCartItems([
+        ...cartItems,
+        { ...produto, quantidade: selectedQuantity },
+      ]);
+    }
+
+    // ... Lógica para adicionar o item ao carrinho
+
+    // Após adicionar o item, exiba a mensagem e defina um temporizador para ocultá-la
+    setIsItemAdded(true);
+    setTimeout(() => {
+      setIsItemAdded(false);
+    }, 5000); // Oculta a mensagem após 5 segundos (ou você pode definir outro valor)
+  };
+
+  const handleRemoveFromCart = (index) => {
+    const updatedCartItems = cartItems.filter(
+      (_, itemIndex) => itemIndex !== index
+    );
+    setCartItems(updatedCartItems);
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    for (const item of cartItems) {
+      total += item.preço * item.quantidade;
+    }
+    return total.toFixed(2);
+  };
+
+  useEffect(() => {
+    setTotal(calculateTotal());
+    // Salva os itens do carrinho no localStorage sempre que o cartItems for modificado
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const LoadingContainer = () => (
     <div className="loading-container">
@@ -117,6 +216,9 @@ const Cart = () => {
           // Handle the case when the product is not found
         }
         setProductLoading(false);
+        if (productData.color && productData.color.length > 0) {
+          setIsAddToCartDisabled(!selectedColor || !selectedSize);
+        }
       } catch (error) {
         console.error("Erro ao buscar dados do produto:", error);
         // Handle the error state appropriately
@@ -125,12 +227,18 @@ const Cart = () => {
     };
 
     fetchProductData();
-  }, [collectionName, productName]);
+  }, [collectionName, productName, productData, selectedColor, selectedSize]);
 
   if (productLoading) {
     return <LoadingContainer />;
   }
 
+  const handleQuantityChange = (newQuantity) => {
+    console.log("New Quantity:", newQuantity);
+    if (newQuantity >= 1) {
+      setSelectedQuantity(newQuantity);
+    }
+  };
   const handleAddToCart1 = () => {
     const data = {
       color: selectedColor,
@@ -156,6 +264,7 @@ const Cart = () => {
           preço: productData ? productData.preço : 0,
           url_image: productData ? productData.url_image : "",
         };
+        console.log(newItem);
         setCart([...cart, newItem]);
       } else {
         alert(
@@ -177,25 +286,23 @@ const Cart = () => {
       }
     }, []); // O segundo argumento vazio garante que este efeito seja executado apenas uma vez, após a montagem inicial do componente.
 
-    const handleQuantityChange = (newQuantity) => {
-      if (newQuantity >= 1) {
-        setSelectedQuantity(newQuantity);
-      }
-    };
-
     const handleAddToCart = (produto) => {
       const existingItemIndex = cartItems.findIndex(
-        (item) => item.nome_prop === produto.nome_prop
+        (item) =>
+          item.nome_prop === produto.nome_prop || item.nome_prop === productName
       );
 
       if (existingItemIndex !== -1) {
         // Se o item já está no carrinho, aumente a quantidade
         const updatedCartItems = [...cartItems];
-        updatedCartItems[existingItemIndex].quantidade += 1;
+        updatedCartItems[existingItemIndex].quantidade += selectedQuantity;
         setCartItems(updatedCartItems);
       } else {
         // Se o item não está no carrinho, adicione-o com quantidade 1
-        setCartItems([...cartItems, { ...produto, quantidade: 1 }]);
+        setCartItems([
+          ...cartItems,
+          { ...produto, quantidade: selectedQuantity },
+        ]);
       }
     };
 
@@ -313,12 +420,93 @@ const Cart = () => {
                 style={{ color: "#ffffff" }}
               ></i>
             </Link>
-            <Link to="/cart">
-              <i
-                className="bx bx-cart bt-header"
-                style={{ color: "#ffffff" }}
-              ></i>
+            <i
+              className="bx bx-cart bt-header animation"
+              style={{ color: "#ffffff" }}
+              id="cart-icon"
+              onClick={handleCartIconClick}
+            ></i>
+          </div>
+
+          <div
+            className={`overlay ${overlayVisible ? "active" : ""}`}
+            onClick={handleCloseCartClick}
+          ></div>
+          <div className={`cart ${cartVisible ? "active" : ""}`}>
+            <h2 className="cart-title-1">MEU CARRINHO</h2>
+            <div className="cart-content-1">
+              {cartItems.map((produto, index) => (
+                <div className="cart-item" key={index}>
+                  <img
+                    src={produto.url_image}
+                    alt={produto.nome_prop}
+                    className="cart-item-image-1"
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      objectFit: "contain",
+                      padding: "10px",
+                      borderRadius: "60px",
+                    }}
+                  />
+
+                  <div className="cart-item-details">
+                    <div className="cart-item-name-1">
+                      {produto.nome_prodmale ||
+                        produto.nome_prop ||
+                        produto.nome_prodpromo ||
+                        produto.nome_prodfemme}
+                    </div>
+                    <div className="cart-item-price-1">R$ {produto.preço}</div>
+                    <input
+                      type="number"
+                      className="cart-quantity-1"
+                      value={produto.quantidade}
+                      onChange={(e) => {
+                        const updatedCartItems = [...cartItems];
+                        updatedCartItems[index].quantidade =
+                          parseInt(e.target.value, 10) || 0;
+                        setCartItems(updatedCartItems);
+                      }}
+                      style={{
+                        border: "1.2px solid #48a3a9",
+                        outlineColor: "#48a3a9",
+                        width: "2.6rem",
+                        textAlign: "center",
+                        fontSize: "0.8rem",
+                      }}
+                    />
+                  </div>
+                  <i
+                    className="bx bxs-trash-alt cart-remove cart-item-remove-1"
+                    onClick={() => handleRemoveFromCart(index)}
+                  ></i>
+                </div>
+              ))}
+            </div>
+            <div className="cart-box"></div>
+            <hr></hr>
+            <div className="total">
+              <div className="total-title">Total</div>
+              <div className="total-price">$ {total}</div>
+            </div>
+
+            <Link to="/checkout">
+              <button type="button" className="btn-buy">
+                COMPRAR AGORA
+              </button>
             </Link>
+
+            <Link to="/cart">
+              <button type="button" className="btn-buy">
+                VER MEU CARRINHO
+              </button>
+            </Link>
+            <i
+              className="bx bx-x"
+              id="close-cart"
+              onClick={handleCloseCartClick}
+            ></i>
           </div>
         </header>
         <div className="container-subheader-1">
@@ -355,20 +543,26 @@ const Cart = () => {
               <div className="imagens">
                 <img
                   className="small-images"
-                  src="/assets/principal.png"
-                  onClick={() => handleImageClick("/assets/principal.png")}
+                  src={productData ? productData.url_image : ""}
+                  onClick={() =>
+                    setSelectedImage(productData ? productData.url_image : "")
+                  }
                 />
 
                 <img
                   className="small-images"
-                  src="/assets/image-2.png"
-                  onClick={() => handleImageClick("/assets/image-2.png")}
+                  src={productData ? productData.url_image2 : ""}
+                  onClick={() =>
+                    setSelectedImage(productData ? productData.url_image2 : "")
+                  }
                 />
 
                 <img
                   className="small-images"
-                  src="/assets/image-3.png"
-                  onClick={() => handleImageClick("/assets/image-3.png")}
+                  src={productData ? productData.url_image3 : ""}
+                  onClick={() =>
+                    setSelectedImage(productData ? productData.url_image3 : "")
+                  }
                 />
               </div>
             </div>
@@ -378,7 +572,7 @@ const Cart = () => {
             <div className="left-side">
               <div className="items">
                 <div className="select-image">
-                  <img src={productData ? productData.url_image : ""} />
+                  <img src={selectedImage} alt="Selected" />
                 </div>
               </div>
             </div>
@@ -506,11 +700,26 @@ const Cart = () => {
                 <button className="buttons-cart">
                   <span>Comprar agora</span>
                 </button>
-                <button className="buttons-cart">
+                <button
+                  className="buttons-cart"
+                  onClick={() => {
+                    handleAddToCart(productData);
+                    showAddedToCartNotification();
+                  }}
+                >
                   <i className="bx bx-cart bt-header"></i>
                   <span>Adicionar ao carrinho</span>
                 </button>
-
+                {showNotification && (
+                  <div
+                    className={`notification ${isItemAdded ? "active" : ""}`}
+                  >
+                    <p className="not">Item adicionado ao carrinho!</p>
+                    <Link to="/cart2" className="go-to-cart-button">
+                      Ir para o Carrinho
+                    </Link>
+                  </div>
+                )}
                 <div className="cep-text">
                   <i
                     className="bx bxs-truck"
@@ -572,17 +781,29 @@ const Cart = () => {
           </div>
 
           <div className="footer-section-div">
-  <h3 className='footer-animation-title'>CONTATOS</h3>
-  <a href="https://web.whatsapp.com/send?phone=5585986056136" target="_blank" title="whatsapp">
-    <i className="fa fa-whatsapp"></i>
-  </a>
-  <a href="https://www.facebook.com/maremansakidss" target="_blank" title="facebook">
-    <i className="fa fa-facebook"></i>
-  </a>
-  <a href="https://www.instagram.com/maremansakids/" target="_blank" title="instagram">
-    <i className="fa fa-instagram"></i>
-  </a>
-</div>
+            <h3 className="footer-animation-title">CONTATOS</h3>
+            <a
+              href="https://web.whatsapp.com/send?phone=5585986056136"
+              target="_blank"
+              title="whatsapp"
+            >
+              <i className="fa fa-whatsapp"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/maremansakidss"
+              target="_blank"
+              title="facebook"
+            >
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/maremansakids/"
+              target="_blank"
+              title="instagram"
+            >
+              <i className="fa fa-instagram"></i>
+            </a>
+          </div>
         </section>
       </footer>
       <div className="last-text">
